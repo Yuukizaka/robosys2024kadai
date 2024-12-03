5dfc8e3d2d91798fdf3e25392cf5fdf057482aeb
Yuuki Ishizaka <s23c1011jk@s.chibakoudai.jp>
2024-12-03 14:48:22 +0900

    test

--- test.bash
+++ test.bash
@@ -9,27 +9,22 @@ ng () {
 
 res=0
 
-# テストケース1: 西暦2023の結果を確認
-out=$(echo 2023 | ./eto.py)
+out=$(echo 2023 | ./eto)
 [ "${out}" = "2023 年の干支は卯です。" ] || ng "$LINENO"
 
-# テストケース2: 西暦2000の結果を確認
-out=$(echo 2000 | ./eto.py)
+out=$(echo 2000 | ./eto)
 [ "${out}" = "2000 年の干支は辰です。" ] || ng "$LINENO"
 
-# テストケース3: 無効な入力の場合
-out=$(echo abc | ./eto.py)
+out=$(echo abc | ./eto)
 [ "${out}" = "無効な入力です。西暦の数字を入力してください。" ] || ng "$LINENO"
 
-# テストケース4: 西暦1987の結果を確認
-out=$(echo 1987 | ./eto.py)
+out=$(echo 1987 | ./eto)
 [ "${out}" = "1987 年の干支は卯です。" ] || ng "$LINENO"
 
-# テストケース5: 西暦1990の結果を確認
-out=$(echo 1990 | ./eto.py)
+out=$(echo 1990 | ./eto)
 [ "${out}" = "1990 年の干支は午です。" ] || ng "$LINENO"
 
-# テスト結果の表示
-[ "${res}" = 0 ] && echo "OK" # 通った場合に表示
+[ "${res}" = 0 ] && echo "OK"
 exit $res
+
 

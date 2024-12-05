413796392b46dff9be41e9e40c0797bfb9a9e68b
Yuuki Ishizaka <s23c1011jk@s.chibakoudai.jp>
2024-12-05 11:06:59 +0900

    完成

--- test.bash
+++ test.bash
@@ -3,28 +3,34 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-        echo "${1}行目が違うよ"
-        res=1
+    echo "${1}行目が違うよ"
+    res=1
 }
 
 res=0
 
-out=$(echo 2023 | ./eto)
+# 2023年のテスト
+out=$(echo 2023 | ./eto2)
 [ "${out}" = "2023 年の干支は卯です。" ] || ng "$LINENO"
 
-out=$(echo 2000 | ./eto)
+# 2000年のテスト
+out=$(echo 2000 | ./eto2)
 [ "${out}" = "2000 年の干支は辰です。" ] || ng "$LINENO"
 
-out=$(echo abc | ./eto)
-[ "${out}" = "無効な入力です。西暦の数字を入力してください。" ] || ng "$LINENO"
+# 無効な入力（abc）のテスト
+out=$(echo abc | ./eto2 2>/dev/null; echo $?)
+[ "${out}" = "1" ] || ng "$LINENO"
 
-out=$(echo 1987 | ./eto)
+# 1987年のテスト
+out=$(echo 1987 | ./eto2)
 [ "${out}" = "1987 年の干支は卯です。" ] || ng "$LINENO"
 
-out=$(echo 1990 | ./eto)
+# 1990年のテスト
+out=$(echo 1990 | ./eto2)
 [ "${out}" = "1990 年の干支は午です。" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo "OK"
 exit $res
+
 
 

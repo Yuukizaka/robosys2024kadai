3f02c866c7395d9ac253766b998e38a3fea07ffa
Yuuki Ishizaka <s23c1011jk@s.chibakoudai.jp>
2024-12-05 11:13:38 +0900

    完成

--- test.bash
+++ test.bash
@@ -10,23 +10,23 @@ ng () {
 res=0
 
 # 2023年のテスト
-out=$(echo 2023 | ./eto2)
+out=$(echo 2023 | ./eto)
 [ "${out}" = "2023 年の干支は卯です。" ] || ng "$LINENO"
 
 # 2000年のテスト
-out=$(echo 2000 | ./eto2)
+out=$(echo 2000 | ./eto)
 [ "${out}" = "2000 年の干支は辰です。" ] || ng "$LINENO"
 
 # 無効な入力（abc）のテスト
-out=$(echo abc | ./eto2 2>/dev/null; echo $?)
+out=$(echo abc | ./eto 2>/dev/null; echo $?)
 [ "${out}" = "1" ] || ng "$LINENO"
 
 # 1987年のテスト
-out=$(echo 1987 | ./eto2)
+out=$(echo 1987 | ./eto)
 [ "${out}" = "1987 年の干支は卯です。" ] || ng "$LINENO"
 
 # 1990年のテスト
-out=$(echo 1990 | ./eto2)
+out=$(echo 1990 | ./eto)
 [ "${out}" = "1990 年の干支は午です。" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo "OK"
